25545c540d74242cd0803f69db7773fad217f8fb
HansiHE <hansihe@hansihe.com>
2016-04-07 01:01:59 +0200

    a test

--- test/mc_ex_test.exs
+++ test/mc_ex_test.exs
@@ -4,4 +4,19 @@ defmodule McExTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "server player join" do
+    world_id = :sw1
+
+    {:ok, supervisor} = McEx.World.Supervisor.start_link(:sw1)
+    {:ok, player_pid} = McEx.World.EntitySupervisor.start_entity(
+      world_id, McEx.Player,
+      %{
+        connection: %McProtocol.Acceptor.ProtocolState.Connection{control: self},
+        identity: %{online: false, name: "Testplayer", uuid: McProtocol.UUID.uuid4},
+        entity_id: 100,
+      }
+    )
+    :ok = Supervisor.stop(supervisor, :normal)
+  end
 end
